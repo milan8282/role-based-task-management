b00a748ebff56d7ee58a84e570689697fd2a58e8
Milan Dhameliya <milandhameliya173@gmail.com>
2026-04-29 18:40:19 +0530

    first commit +

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,14 +1,7 @@
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAgb6mJMy0Sk1ZrYNs_pEt5Bw0W_mmBbzo",
-  authDomain: "task-management-5ccaa.firebaseapp.com",
-  projectId: "task-management-5ccaa",
-  storageBucket: "task-management-5ccaa.firebasestorage.app",
-  messagingSenderId: "493883723517",
-  appId: "1:493883723517:web:004b4e98e5b607a8f67894",
-});
+
 
 const messaging = firebase.messaging();
 
